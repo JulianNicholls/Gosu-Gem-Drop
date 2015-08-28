2bc52a5587145e287eb53bccf0bced144f7c8073
Julian Nicholls <juliannicholls29@gmail.com>
2015-08-28 21:59:32 +0100

    Add grid functions.
    Add and remove gem. both take a place which can be a Grid::Point or an
    index.
    Unused process_click().
    new_waiting_gem() adds a gem above the game board ready for dropping.
    to_index() examiness its parameter and only converts it if it's a
    Grid::Point to begin with.
    valid?() now allows for the y co-ordinate to be -1, i.e. the waiting
    gem.

--- grid.rb
+++ grid.rb
@@ -9,18 +9,53 @@ module ColumnDrop
     def initialize(drawer)
       @grid = Array.new(7 * 7, 0)
       @drawer = drawer
+      @waiting_gem = 0
+    end
+
+    def add_gem(place, value)
+      place = to_index(place)
+
+      @grid[place] = value
+    end
+
+    # A gem may not be removed, if it is a blank gem, in which case it will
+    # be transformed into a numbered gem
+    def remove_gem(place)
+      place = to_index(place)
+
+      value = @grid[place]
+      @grid[place] = [0. value - 8].max
     end
 
     def draw
       @grid.each_with_index do |cell, index|
         @drawer.draw_gem(to_gpoint(index), cell)
       end
+
+      return if @waiting_gem == 0
+
+      @drawer.draw_gem(Grid::Point.new(0, -1), @waiting_gem)
+    end
+
+    def process_click(point)
+      return unless point.x.between?(BOARD_LEFT + 7 * TILE) &&
+        point.y.between?(BOARD_TOP - TILE, BOARD_TOP + 7 * TILE)
+
+      # Do something with click
+    end
+
+    def new_waiting_gem(value = nil)
+      return if @waiting_gem != 0
+
+      @waiting_gem = value || rand(1..7)
+      @waiting_gem += BLANK_FLAG if rand(1..8) == 1
     end
 
     private
 
-    def to_index(gpoint)
-      gpoint.y * 7 + gpoint.x
+    # Convert a value to an index into the grid, if it is a grid point.
+    def to_index(place)
+      place.respond_to?(:x) ? place.y * 7 + place.x : place
     end
 
     def to_gpoint(index)
@@ -36,10 +71,15 @@ module ColumnDrop
           .offset(x * TILE, y * TILE)
       end
 
+      # Can only be called if point is within the board, or in the well above
+      def from_point(point)
+        new(point.x - BOARD_LEFT, point.y - BOARD_TOP)
+      end
+
       private
 
       def valid?
-        x.between?(0, 6) && y.between?(0, 6)
+        x.between?(0, 6) && y.between?(-1, 6)   # -1 is above the columns
       end
     end
   end
