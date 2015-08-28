0fdf32a8c6a0c21ab4bf61491185f878e0eb28a4
Julian Nicholls <juliannicholls29@gmail.com>
2015-08-28 22:06:34 +0100

    Add necessary callbacks.
    Add caption and needs_cursor?().
    Add a temporary buttondown() which exits on Escape key.
    Temporary fill of a line and column, and a blank gem.

--- column.rb
+++ column.rb
@@ -17,13 +17,30 @@ module ColumnDrop
     def initialize
       super(WIDTH, HEIGHT)
 
+      self.caption = "Goody Goody Gem Drop"
+
       @drawer = Drawer.new(self)
 
       reset
+
+      # TEMPorary
+
+      7.times do |value|
+        @grid.add_gem(Grid::Point.new(value , 6), value + 1)
+        @grid.add_gem(Grid::Point.new(0, value), value + 1)
+      end
+
+      @grid.add_gem(36, 12)
+      @grid.new_waiting_gem
     end
 
     def reset
       @grid = Grid.new(@drawer)
+    end
+
+    # Indicate that we need the mouse cursor
+    def needs_cursor?
+      true
     end
 
     def update
@@ -35,6 +52,10 @@ module ColumnDrop
       @grid.draw
 
       run_animations
+    end
+
+    def button_down(btn_id)
+      close if btn_id == Gosu::KbEscape
     end
 
     def add_animation(speed, &block)
