c220c957c0555330aeed9e2b067f916175577d61
Julian Nicholls <juliannicholls29@gmail.com>
2015-08-28 21:57:45 +0100

    Lengthen header and add flag for blank gem.

--- constants.rb
+++ constants.rb
@@ -2,7 +2,7 @@ module ColumnDrop
   # Constants
   module Constants
     MARGIN    = 5
-    HEADER    = 20
+    HEADER    = 40
 
     BOARD     = 280
     TILE      = 40
@@ -12,6 +12,8 @@ module ColumnDrop
 
     BOARD_TOP       = 3 * MARGIN + HEADER + TILE
     BOARD_LEFT      = MARGIN
+
+    BLANK_FLAG      = 8
 
     MARGIN_COLOUR   = Gosu::Color.new(0xff_303030)
     BOARD_COLOUR    = Gosu::Color::BLACK
